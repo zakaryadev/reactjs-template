570fe3d1c4b9c3e33bd0ac4f8e9670e75ce305eb
Zakarya Muratbaev Baxtiyar uli <99710957+zakaryadev@users.noreply.github.com>
2025-01-09 22:09:09 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template',
+  base: '/',
   plugins: [
     // Allows using React dev server along with building a React application with Vite.
     // https://npmjs.com/package/@vitejs/plugin-react-swc
